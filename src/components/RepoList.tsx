403b46b0de3fe521df8b06d9c8922562f6258cca
Ricky Jiang <mr.jiangxue@hotmail.com>
2023-08-11 16:24:52 +0800

    added motion module to handle animation

--- src/components/RepoList.tsx
+++ src/components/RepoList.tsx
@@ -1,8 +1,22 @@
 import { useGitHubRepoSearch } from "@/hooks";
+import { motion } from "framer-motion";
 
 interface IRepoListProps {
   searchQuery: string
 }
+
+let parent = {
+  show: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+let stat = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
 
 export default function RepoList({ searchQuery }: IRepoListProps) {
   const { data } = useGitHubRepoSearch(searchQuery);
@@ -12,18 +26,27 @@ export default function RepoList({ searchQuery }: IRepoListProps) {
   }
 
   return (
-    <ul className="w-full">
+    <motion.ul
+      variants={parent}
+      initial="show"
+      animate="show"
+      className="w-full"
+    >
       {
-        data && data.items.map((repo) => (
-          <li
+        data && data.items.map((repo, index) => (
+          <motion.li
             key={repo.id}
+            variants={stat}
+            initial="hidden"
+            animate="show"
+            transition={{ delay: index * 0.1 }}
             className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4 dark:border-opacity-50">
             <h2 className="font-bold">{repo.name}</h2>
             <a href={repo.html_url} target="_blank" className="underline">{repo.html_url}</a>
             <p>{repo.owner?.login}</p>
-          </li>
+          </motion.li>
         ))
       }
-    </ul>
+    </motion.ul>
   )
 }
